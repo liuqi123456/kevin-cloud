83c288dc13e831973f46c517965a1b9de9e8df70
liuqi <liuqi@email.jingqb.com>
2024-06-25 11:12:41 +0800

    first commit

---
+++ kevin-modules/kevin-job/src/main/java/org/kevin/job/controller/SysJobLogController.java
@@ -0,0 +1,87 @@
+package org.kevin.job.controller;
+
+import org.kevin.common.core.utils.poi.ExcelUtil;
+import org.kevin.common.core.web.controller.BaseController;
+import org.kevin.common.core.web.domain.AjaxResult;
+import org.kevin.common.core.web.page.TableDataInfo;
+import org.kevin.common.log.annotation.Log;
+import org.kevin.common.log.enums.BusinessType;
+import org.kevin.common.security.annotation.RequiresPermissions;
+import org.kevin.job.domain.SysJobLog;
+import org.kevin.job.service.ISysJobLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * 调度日志操作处理
+ * 
+ * @author kevin
+ */
+@RestController
+@RequestMapping("/job/log")
+public class SysJobLogController extends BaseController
+{
+    @Autowired
+    private ISysJobLogService jobLogService;
+
+    /**
+     * 查询定时任务调度日志列表
+     */
+    @RequiresPermissions("monitor:job:list")
+    @GetMapping("/list")
+    public TableDataInfo list(SysJobLog sysJobLog)
+    {
+        startPage();
+        List<SysJobLog> list = jobLogService.selectJobLogList(sysJobLog);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出定时任务调度日志列表
+     */
+    @RequiresPermissions("monitor:job:export")
+    @Log(title = "任务调度日志", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, SysJobLog sysJobLog)
+    {
+        List<SysJobLog> list = jobLogService.selectJobLogList(sysJobLog);
+        ExcelUtil<SysJobLog> util = new ExcelUtil<SysJobLog>(SysJobLog.class);
+        util.exportExcel(response, list, "调度日志");
+    }
+
+    /**
+     * 根据调度编号获取详细信息
+     */
+    @RequiresPermissions("monitor:job:query")
+    @GetMapping(value = "/{jobLogId}")
+    public AjaxResult getInfo(@PathVariable Long jobLogId)
+    {
+        return success(jobLogService.selectJobLogById(jobLogId));
+    }
+
+    /**
+     * 删除定时任务调度日志
+     */
+    @RequiresPermissions("monitor:job:remove")
+    @Log(title = "定时任务调度日志", businessType = BusinessType.DELETE)
+    @DeleteMapping("/{jobLogIds}")
+    public AjaxResult remove(@PathVariable Long[] jobLogIds)
+    {
+        return toAjax(jobLogService.deleteJobLogByIds(jobLogIds));
+    }
+
+    /**
+     * 清空定时任务调度日志
+     */
+    @RequiresPermissions("monitor:job:remove")
+    @Log(title = "调度日志", businessType = BusinessType.CLEAN)
+    @DeleteMapping("/clean")
+    public AjaxResult clean()
+    {
+        jobLogService.cleanJobLog();
+        return success();
+    }
+}
---
+++ kevin-modules/kevin-job/src/main/java/org/kevin/job/exception/QuartzDisallowConcurrentExecution.java
@@ -0,0 +1,23 @@
+package org.kevin.job.exception;
+
+import org.kevin.job.domain.SysJob;
+import org.kevin.job.utils.AbstractQuartzJob;
+import org.kevin.job.utils.JobInvokeUtil;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
+
+/**
+ * 定时任务处理（禁止并发执行）
+ * 
+ * @author kevin
+ *
+ */
+@DisallowConcurrentExecution
+public class QuartzDisallowConcurrentExecution extends AbstractQuartzJob
+{
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception
+    {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
